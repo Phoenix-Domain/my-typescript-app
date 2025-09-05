f44002aa7d10c5a8e4f2809c888bbb6b9df46ae1
Batubo Victory <victorybatubo76@gmail.com>
2025-09-05 13:01:38 +0100

    feat: add text and buttons to count component

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,7 +1,30 @@
+import { useState } from 'react';
+
 function Counter(){
+  const [count, setCount] = useState<number>(0)
   return(
     <>
-    
+      <p className='text-2xl font-bold'>Count: {count}</p>
+
+      <div className='flex w-fit gap-5 my-3'>
+        <button type='button' className='bg-[rgb(53,51,51)] text-white px-4 py-2 rounded-full' onClick={() => {
+          setCount(prevVal => prevVal + 1)
+        }}>
+          Increment
+        </button>
+
+        <button type='button' className='bg-[rgb(53,51,51)] text-white px-4 py-2 rounded-full' onClick={() => {
+          setCount(prevVal => prevVal - 1)
+        }}>
+          Decrement
+        </button>
+
+        <button type='button' className='bg-[rgb(53,51,51)] text-white px-4 py-2 rounded-full' onClick={() => {
+          setCount(0)
+        }}>
+          Reset
+        </button>
+      </div>
     </>
   )
 }
