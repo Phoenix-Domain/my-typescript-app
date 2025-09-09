382044998900550ea4eef001e6e93c7f8d25c426
Batubo Victory <victorybatubo76@gmail.com>
2025-09-09 13:30:33 +0100

    feat: Render results div only when name, email and input fields are submitted

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -8,23 +8,31 @@ function Form(){
     return(
         <>
             <form action="" className="flex flex-col w-fit p-2">
-                <label htmlFor="">Name:</label>
-                <input type="text" className="outline-0 p-1 bg-gray-300 mb-4 mt-2" />
+                <label htmlFor="name">Name:</label>
+                <input id='name' type="text" className="outline-0 p-1 bg-gray-300 mb-4 mt-2" />
 
-                <label htmlFor="">Email:</label>
-                <input type="text" className="outline-0 p-1 bg-gray-300 mb-4 mt-2" />
+                <label htmlFor="email">Email:</label>
+                <input id='email' type="text" className="outline-0 p-1 bg-gray-300 mb-4 mt-2" />
 
-                <label htmlFor="">Message:</label>
-                <input type="text" className="outline-0 p-1 bg-gray-300 mb-4 mt-2" />
+                <label htmlFor="message">Message:</label>
+                <input id='message' type="text" className="outline-0 p-1 bg-gray-300 mb-4 mt-2" />
 
                 <button type='submit' className="bg-blue-700 px-4 py-2 w-fit text-white active:bg-purple-700">
                     Submit
                 </button>
             </form>
 
-            <div id="result">
+            {
+                    name && email && msg && (
+                        <div id="result">
+                            <p>Name: {name}</p>
+                            <p>Email: {email}</p>
+                            <p>Message: {msg}</p>
+                        </div>
+                    )
+                }
+
             
-            </div>
         </>
     )
 }
