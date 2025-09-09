37cd4480110bea337e74dcf135fd025f3d7aade1
Batubo Victory <victorybatubo76@gmail.com>
2025-09-09 13:20:15 +0100

    feat: Add submit button to form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -10,10 +10,14 @@ function Form(){
 
                 <label htmlFor="">Message:</label>
                 <input type="text" className="outline-0 p-1 bg-gray-300 mb-4 mt-2" />
+
+                <button type='submit' className="bg-blue-700 px-4 py-2 w-fit text-white active:bg-purple-700">
+                    Submit
+                </button>
             </form>
 
             <div id="result">
-                
+
             </div>
         </>
     )
