634675d759e3cf5d7d0d763cb7e9f9edb51c130e
Batubo Victory <victorybatubo76@gmail.com>
2025-09-09 13:49:50 +0100

    feat: Complete form with useRef

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -5,21 +5,34 @@ function Form(){
     const [email, setEmail] = useState<string>('')
     const [msg, setMsg] = useState<string>('');
 
-    function handleSubmit(): void{
+    const inputName = useRef<HTMLInputElement>(null);
+    const inputEmail = useRef<HTMLInputElement>(null);
+    const inputMsg = useRef<HTMLInputElement>(null);
 
+    function handleSubmit(): void{
+        const nameVal: string = inputName.current?.value ?? '';
+        const emailVal: string = inputEmail.current?.value ?? '';
+        const msgVal: string = inputMsg.current?.value ?? '';
+
+        setName(nameVal);
+        setEmail(emailVal);
+        setMsg(msgVal);
     }
 
     return(
         <>
-            <form action="" className="flex flex-col w-fit p-2">
+            <form action="" className="flex flex-col w-fit p-2" onSubmit={e => {
+                e.preventDefault();
+                handleSubmit();
+            }}>
                 <label htmlFor="name">Name:</label>
-                <input id='name' type="text" className="outline-0 p-1 bg-gray-300 mb-4 mt-2" />
+                <input id='name' ref={inputName} type="text" className="outline-0 p-1 bg-gray-300 mb-4 mt-2" />
 
                 <label htmlFor="email">Email:</label>
-                <input id='email' type="text" className="outline-0 p-1 bg-gray-300 mb-4 mt-2" />
+                <input id='email' ref={inputEmail} type="text" className="outline-0 p-1 bg-gray-300 mb-4 mt-2" />
 
                 <label htmlFor="message">Message:</label>
-                <input id='message' type="text" className="outline-0 p-1 bg-gray-300 mb-4 mt-2" />
+                <input id='message' ref={inputMsg} type="text" className="outline-0 p-1 bg-gray-300 mb-4 mt-2" />
 
                 <button type='submit' className="bg-blue-700 px-4 py-2 w-fit text-white active:bg-purple-700">
                     Submit
