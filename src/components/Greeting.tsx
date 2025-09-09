bbb88424dfb6330829e05c9107a3ba4b7a7c118f
Batubo Victory <victorybatubo76@gmail.com>
2025-09-09 13:01:00 +0100

    feat: Pass setCount function to button's onClick attribute

--- src/components/Greeting.tsx
+++ src/components/Greeting.tsx
@@ -12,13 +12,19 @@ function Greeting({name}: GreetingProps){
             <p> your count is {count}.</p>
 
             <div className="my-4 flex gap-5">
-                <button className="bg-blue-600 px-4 py-2 rounded-full text-white active:bg-purple-700">
+                <button className="bg-blue-600 px-4 py-2 rounded-full text-white active:bg-purple-700" onClick={() => {
+                    setCount(prevVal => prevVal + 1)
+                }}>
                     Add 1
                 </button>
-                <button className="bg-blue-600 px-4 py-2 rounded-full text-white active:bg-purple-700">
+                <button className="bg-blue-600 px-4 py-2 rounded-full text-white active:bg-purple-700" onClick={() => {
+                    setCount(prevVal => prevVal - 1)
+                }}>
                     Add 2
                 </button>
-                <button className="bg-blue-600 px-4 py-2 rounded-full text-white active:bg-purple-700">
+                <button className="bg-blue-600 px-4 py-2 rounded-full text-white active:bg-purple-700" onClick={() => {
+                    setCount(0)
+                }}>
                     Add 3
                 </button>
             </div>
