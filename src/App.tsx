5d12756bbc0ea9712b23244c88b3d13314be7aec
Batubo Victory <victorybatubo76@gmail.com>
2025-09-23 13:06:13 +0100

    refactor: restart tutorial

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
 import './App.css';
-import Navbar from './components/Navbar';
-import { useContext } from 'react';
-import { ThemeContext } from './components/context/ThemeContext/ThemeContext';
 
 function App() {
-    const {theme, setTheme} = useContext(ThemeContext);
-
        
  return(
-  <div className={`w-[100dvw] h-[100dvh] ${theme === 'dark' ? 'bg-gray-900 text-white' : ''}`}>
-    <Navbar />
-  </div>
+ <>
+ 
+ </>
  )
 }
 
