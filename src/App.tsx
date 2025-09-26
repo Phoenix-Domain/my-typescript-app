251dd029e753dbf30f52583867344cceca4c836d
Batubo Victory <victorybatubo76@gmail.com>
2025-09-26 13:03:53 +0100

    feat: Create custom hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import './App.css';
+import { useState } from 'react';
+import useLocalStorage from './hooks/useLocalStorage';
 
 function App() {
-       
+       const [name, setName] = useState('')
  return(
  <>
- 
+    <input type="text" value={name} onChange={(e) => setName(e.target.value)} className='border bg-gray-700 text-white m-5 p-1'/>
+
+    <p>{name}</p>
  </>
  )
 }
