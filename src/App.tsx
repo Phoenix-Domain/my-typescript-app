855c78e6d248bee7ce28e4e11b557bfe20768aa9
Batubo Victory <victorybatubo76@gmail.com>
2025-09-26 14:20:40 +0100

    refactor: Restart tutorial

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
 import './App.css';
-import { useState } from 'react';
-import useLocalStorage from './hooks/useLocalStorage';
 
 function App() {
-       const [name, setName] = useState('')
  return(
  <>
-    <input type="text" value={name} onChange={(e) => setName(e.target.value)} className='border bg-gray-700 text-white m-5 p-1'/>
-
-    <p>{name}</p>
+    
  </>
  )
 }
--- src/hooks/useLocalStorage.ts
+++
@@ -1,7 +0,0 @@
-import { useState } from 'react';
-
-export default function useLocalStorage(initialValue){
-    const [value, setValue] = useState(initialValue)
-
-    return value
-}
