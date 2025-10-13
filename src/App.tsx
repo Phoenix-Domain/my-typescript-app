b46d9bd0b7fefe03b9c1b624481f748f03af9a74
Batubo Victory <victorybatubo76@gmail.com>
2025-10-13 12:07:38 +0100

    refactor: Move on to the next project

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,10 @@
 import './App.css';
-import Suscribe from './components/Suscribe';
-import Display from './components/Display';
-import { useState } from 'react';
 
 function App() {
-    const [isSuscribed, setIsSuscribed] = useState<boolean>(false);
-
-    const handleSubscription = (): void => {
-        return isSuscribed ? setIsSuscribed(false) : setIsSuscribed(true)
-    }
-
+    
  return(
     <>
-        <Display isSuscribed={isSuscribed}/>
-        <Suscribe handleSubscription={handleSubscription}/>
+       
     </>
  )
 }
