a1c3830a54e2413b1abeef1910228a6378fed3ae
Batubo Victory <victorybatubo76@gmail.com>
2025-10-13 11:28:56 +0100

    refactor: Restart tutorial

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,10 @@
 import './App.css';
-import Card from './components/Card';
 
 function App() {
  return(
-<div className='flex flex-col gap-5'>
-    <Card 
-        title='Batubo Victory'
-        role='Frontend Developer'
-        quote='I turn pixels into experiences that people love.'
-    />
-
-    <Card 
-        title='Ada Christopher'
-        role='UX Designer'
-        quote='Design isn’t just how it looks — it’s how it works.'
-    />
-
-    <Card 
-        title='John Smith'
-        role='Project Manager'
-        quote='Clear goals, strong teams, and no surprises — that’s my mantra.'
-    />
-</div>
+    <>
+    
+    </>
  )
 }
 
