8036b5dbfe279d30d06e8f4b9678ec06c3dece91
Batubo Victory <victorybatubo76@gmail.com>
2025-10-13 11:27:46 +0100

    feat: Create teams card

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,25 @@ import Card from './components/Card';
 
 function App() {
  return(
-<>
+<div className='flex flex-col gap-5'>
     <Card 
         title='Batubo Victory'
-        bio='Hi, I’m Batubo Victory — a frontend developer focused on building responsive, accessible React apps. I value clean code, great UX, and performance. Let’s build something great.'
+        role='Frontend Developer'
+        quote='I turn pixels into experiences that people love.'
     />
-</>
+
+    <Card 
+        title='Ada Christopher'
+        role='UX Designer'
+        quote='Design isn’t just how it looks — it’s how it works.'
+    />
+
+    <Card 
+        title='John Smith'
+        role='Project Manager'
+        quote='Clear goals, strong teams, and no surprises — that’s my mantra.'
+    />
+</div>
  )
 }
 
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,18 +2,25 @@ import Button from './Button';
 
 interface ButtonProps{
     title: string;
-    bio: string
+    quote: string;
+    role: string;
 }
 
-function Card({title, bio}: ButtonProps){
+function Card({title, quote, role}: ButtonProps){
     return(
         <section className='flex flex-col bg-blue-200 items-center gap-5 max-w-[50%] p-5 m-auto rounded-2xl'>
             <h1 className='text-3xl font-bold'>
                 {title}
             </h1>
-            <p>
-                {bio}
+
+            <p className='text-xl font-semibold'>
+                {role}
             </p>
+
+            <p className='italic'>
+                {`"${quote}"`}
+            </p>
+
             <article className='flex gap-10'>
                 <Button 
                     type='button'
