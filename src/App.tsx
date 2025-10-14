95b00171fdf3147cde71b62bbf9dcbcff90b6f3c
Batubo Victory <victorybatubo76@gmail.com>
2025-10-14 14:05:54 +0100

    refactor: Restart the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,11 @@
 import './App.css';
-import Products from './components/Products';
-import Form from './components/Form';
-import { useState, useEffect } from 'react';
-interface Product{
-    id: number;
-    Product: string;
-    Type: string;
-}
+
 
 function App() {
-    const [products, setProducts] = useState<Product[]>(() => {
-        const saved = localStorage.getItem('products');
-        return saved ? JSON.parse(saved) : []
-    });
-
-    useEffect(() => {
-        localStorage.setItem('products', JSON.stringify(products));
-    }, [products])
-
+    
  return(
     <>
-        <h1 className='text-3xl'>
-            Total Products: <span className='text-blue-700 font-bold'>{products.length}</span>
-        </h1>
-       <Form setProducts={setProducts}/>
-       {
-        products.length <= 0 ?  <p>No products yet</p> : (products.map((product, key) => <Products key={key} product={product.Product} type={product.Type} id={product.id} setProducts={setProducts}/>)) 
-       }
+       
     </>
  )
 }
